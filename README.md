98850b40618ba3bc9ff6e2b8bc33564a57ff9d61
Adil Myktybek <adilmyktybek@Adils-MacBook-Pro.local>
2023-02-04 00:57:21 +0600

    [AM-BusRoute] - Added method to read multipartfile data

---
+++ README.md
@@ -0,0 +1,13 @@
+# Getting Started
+
+```shell
+docker-compose -f docker-compose-local.yml up -d
+```
+
+```shell
+docker stop $(docker ps -aq)
+```
+
+```shell
+docker rmi -f $(docker images -q)
+```
--- src/main/java/kz/axelrod/busroute/service/RouteService.java
+++ src/main/java/kz/axelrod/busroute/service/RouteService.java
@@ -5,6 +5,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.IntStream;
+
 @Slf4j
 @Service
 public class RouteService {
@@ -15,14 +24,34 @@ public class RouteService {
         this.redisService = redisService;
     }
 
-    public ResultDto getResultDto(String from, String to, MultipartFile file){
-        log.info("ROUTE-SERVICE --- from={}, to={}, file={}", from, to, file);
+    public ResultDto getResultDto(String from, String to, MultipartFile file) {
+        var fileData = getFileData(file);
+        log.info("RS-getResultDto --- from={}, to={}, file-data={}", from, to, fileData);
         var resultDto = getResultDto(from, to);
         resultDto.setDirect(true);
         return resultDto;
     }
 
-    private ResultDto getResultDto(String from, String to){
+    private Map<Integer, List<Integer>> getFileData(MultipartFile file) {
+        Map<Integer, List<Integer>> routes = new HashMap<>();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
+            reader.lines().map(line -> line.split(" "))
+                    .forEachOrdered(tokens -> {
+                        int from = Integer.parseInt(tokens[0]);
+                        IntStream.range(1, tokens.length).map(i -> Integer.parseInt(tokens[i]))
+                                .forEachOrdered(to -> {
+                                    if (!routes.containsKey(from)) routes.put(from, new ArrayList<>());
+                                    routes.get(from).add(to);
+                                });
+                    });
+        } catch (IOException e) {
+            log.error("An error occurred while reading the Multipart File.", e);
+        }
+        log.info("RS-getFileData --- routes {}", routes);
+        return routes;
+    }
+
+    private ResultDto getResultDto(String from, String to) {
         var resultDto = new ResultDto();
         resultDto.setFrom(from);
         resultDto.setTo(to);
